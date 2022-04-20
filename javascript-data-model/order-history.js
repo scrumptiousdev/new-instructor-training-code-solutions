6ac7454b7912682f318070c779fa54b0c53f87d0
ScrumptiousDev <ryan.mingyu.choi@gmail.com>
2022-04-20 22:46:11 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,141 @@
+var orderHistory = [
+  {
+    orderNumber: '114-3941689-8772232',
+    orderedAtUTC: 1596499200,
+    displayReviewUI: true,
+    displaySupportUI: false,
+    total: {
+      amount: 34,
+      currency: 'USD'
+    },
+    shipTo: {
+      name: 'JS Masher',
+      link: 'https://amazon.com/order-ship-to/js-masher'
+    },
+    delivery: {
+      isDelivered: true,
+      deliveredAtUTC: 1596844800,
+      note: null
+    },
+    items: [
+      {
+        itemType: 'book',
+        itemNumber: 19847328147,
+        title: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        returnWindowDateUTC: 1599436800,
+        price: {
+          amount: 31.55,
+          currency: 'USD'
+        }
+      }
+    ]
+  },
+  {
+    orderNumber: '113-9984268-1280257',
+    orderedAtUTC: 1595116800,
+    displayReviewUI: true,
+    displaySupportUI: false,
+    total: {
+      amount: 44.53,
+      currency: 'USD'
+    },
+    shipTo: {
+      name: 'JS Masher',
+      link: 'https://amazon.com/order-ship-to/js-masher'
+    },
+    delivery: {
+      isDelivered: true,
+      deliveredAtUTC: 1595203200,
+      note: 'Your package was delivered. It was handed directly to a resident.'
+    },
+    items: [
+      {
+        itemType: 'book',
+        itemNumber: 12463,
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnWindowDateUTC: 1597795200,
+        price: {
+          amount: 41.33,
+          currency: 'USD'
+        }
+      }
+    ]
+  },
+  {
+    orderNumber: '114-2875557-9059409',
+    orderedAtUTC: 1593820800,
+    displayReviewUI: true,
+    displaySupportUI: false,
+    total: {
+      amount: 17.22,
+      currency: 'USD'
+    },
+    shipTo: {
+      name: 'JS Masher',
+      link: 'https://amazon.com/order-ship-to/js-masher'
+    },
+    delivery: {
+      isDelivered: true,
+      deliveredAtUTC: 1594080000,
+      note: 'Your package was delivered. It was handed directly to a resident.'
+    },
+    items: [
+      {
+        itemType: 'hardware',
+        itemNumber: 86346,
+        title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        author: null,
+        returnWindowDateUTC: 1596585600,
+        price: {
+          amount: 15.98,
+          currency: 'USD'
+        }
+      }
+    ]
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    orderedAtUTC: 1593734400,
+    displayReviewUI: true,
+    displaySupportUI: true,
+    total: {
+      amount: 138.93,
+      currency: 'USD'
+    },
+    shipTo: {
+      name: 'JS Masher',
+      link: 'https://amazon.com/order-ship-to/js-masher'
+    },
+    delivery: {
+      isDelivered: true,
+      deliveredAtUTC: 1593907200,
+      note: null
+    },
+    items: [
+      {
+        itemType: 'hardware',
+        itemNumber: 51515,
+        title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        author: null,
+        returnWindowDateUTC: 1596499200,
+        price: {
+          amount: 94.95,
+          currency: 'USD'
+        }
+      },
+      {
+        itemType: 'book',
+        itemNumber: 85896,
+        title: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        returnWindowDateUTC: 1596499200,
+        price: {
+          amount: 33.99,
+          currency: 'USD'
+        }
+      }
+    ]
+  }
+];
