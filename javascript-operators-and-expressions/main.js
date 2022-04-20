7381040e3492732ea6d7e931fc712a62d15fe4b2
ScrumptiousDev <ryan.mingyu.choi@gmail.com>
2022-04-20 21:48:40 +0000

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,45 @@
+const width = 200;
+const height = 100;
+const area = width * height;
+
+console.log(`value of area: ${area}`);
+console.log(`typeof area: ${typeof area}`);
+
+const bill = 100;
+const payment = 500;
+const change = payment - bill;
+
+console.log(`value of change: ${change}`);
+console.log(`typeof change: ${typeof change}`);
+
+const quizzes = 100;
+const midterm = 90;
+const final = 88;
+const grade = (quizzes + midterm + final) / 3;
+
+console.log(`value of grade: ${grade}`);
+console.log(`typeof grade: ${typeof grade}`);
+
+const firstName = 'Ryan';
+const lastName = 'Choi';
+const fullName = `${firstName} ${lastName}`;
+
+console.log(`value of fullName: ${fullName}`);
+console.log(`typeof fullName: ${typeof fullName}`);
+
+const pH = 10;
+const isAcidic = pH < 7;
+
+console.log(`value of isAcidic: ${isAcidic}`);
+console.log(`typeof isAcidic: ${typeof isAcidic}`);
+
+const headCount = 20;
+const isSparta = headCount === 300;
+
+console.log(`value of isSparta: ${isSparta}`);
+console.log(`typeof isSparta: ${typeof isSparta}`);
+
+const motto = fullName + ' is the GOAT';
+
+console.log(`value of motto: ${motto}`);
+console.log(`typeof motto: ${typeof motto}`);
