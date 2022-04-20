6bba6099a0d5e0306d8cb42f58ff19a77c2561bd
ScrumptiousDev <ryan.mingyu.choi@gmail.com>
2022-04-20 21:37:35 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,17 @@
+const fullName = 'Ryan Choi';
+const isCool = true;
+const totalPets = 1;
+const unknown = null;
+let never;
+
+console.log(`value of fullName: ${fullName}`);
+console.log(`value of isCool: ${isCool}`);
+console.log(`value of totalPets: ${totalPets}`);
+console.log(`value of unknown: ${unknown}`);
+console.log(`value of never: ${never}`);
+
+console.log(`typeof fullName: ${typeof fullName}`);
+console.log(`typeof isCool: ${typeof isCool}`);
+console.log(`typeof totalPets: ${typeof totalPets}`);
+console.log(`typeof unknown: ${typeof unknown}`);
+console.log(`typeof never: ${typeof never}`);
